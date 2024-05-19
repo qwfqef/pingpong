1864e3358b90365dc7988a364419ad282bace777
vantipin <168963114+qwfqef@users.noreply.github.com>
2024-05-19 14:45:21 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ fps = 60
 clock = time.Clock()
 mixer.music.load('space.ogg')
 font.init()
-font2 = font.SysFont('Aerial', 36)
-font1 = font.SysFont('Aerial', 36)
-win = font1.render('YOU WIN!', True, (0, 0, 0))
-lose = font1.render('YOU LOSE!', True, (0, 0, 0))
+font1 = font.SysFont(None, 35)
+lose1 = font1.render('PLAYER 1 LOSE', True, (180, 0, 0))
+lose2 = font1.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_speed, player_x, player_y, size_x, size_y):
         super().__init__()
@@ -42,21 +41,37 @@ class Player(GameSprite):
 
 racket = Player('saassadasd.png', 5, 5, 300, 10, 100)
 racket2 = Player('saassadasd.png', 5, 685, 300, 10, 100)
-ball = GameSprite('png-transparent-tennis-ball-tennis-balls-racket-drawing-football-green-yellow-circle-Photoroom.png-Photoroom.png', 4, 250, 350, 50, 50)
+ball = GameSprite('png-transparent-tennis-ball-tennis-balls-racket-drawing-football-green-yellow-circle-Photoroom.png-Photoroom.png', 6, 250, 350, 50, 50)
 
 finish = False
-
+speed_x = 3
+speed_y = 3
 while game:
     window.blit(background, (0,0))
     for e in event.get():
         if e.type == QUIT:
             game = False
-    racket.update_l()
-    racket2.update_r()
-    racket.reset()
-    racket2.reset()
-    ball.reset()
-    ball.update()
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        racket.update_l()
+        racket2.update_r()
+        racket.reset()
+        racket2.reset()
+        ball.reset()
+        ball.update()
+    if ball.rect.y > window_height-250 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200,200))
+        game_over = True
+    if ball.rect.x > 650:
+        finish = True
+        window.blit(lose2, (200,200))
+        game_over = True
     display.update()
     clock.tick(fps)
-        
+    
